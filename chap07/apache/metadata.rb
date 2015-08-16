1441c20c742b8b834fdc535033142c9a75e4c571
Iliya Strakovich <iliya@strakovich.com>
2015-08-16 16:33:33 +0300

    chap07

---
+++ chap07/apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'Iliya Strakovich'
+maintainer_email 'iliya@strakovich.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
